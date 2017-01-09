2206dae090d5a00a5e1a0c4cbaa0ae41175f70d0
xiaoliufu <xiaoliufu@meituan.com>
2017-01-09 19:11:01 +0800

    server端解决半包问题

--- src/main/java/com/xlf/demo/server/EchoServer.java
+++ src/main/java/com/xlf/demo/server/EchoServer.java
@@ -8,6 +8,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,6 +34,8 @@ public class EchoServer {
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                            socketChannel.pipeline().addLast(new StringDecoder());
                             socketChannel.pipeline().addLast(new EchoServerHandler());
                         }
                     })
